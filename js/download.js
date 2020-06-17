250a827d0f052cb5ec33fd891886ccb7e8beefcb
LiNCHx <linchde@outlook.de>
2020-06-18 00:03:04 +0200

    Catch more errors

--- js/download.js
+++ js/download.js
@@ -21,8 +21,16 @@ function DownloadList() {
 // Create and Download playerlist.tf2bl.json
 function DownloadJSON() {
     $.get(BOT_LIST_URL, function (data) {
-        let jsonString = ListToTF2B(data);
-        if (jsonString) { DownloadFile("playerlist.tf2bl.json", jsonString); }
+        try {
+            let jsonString = ListToTF2B(data);
+            if (jsonString) { DownloadFile("playerlist.tf2bl.json", jsonString); }
+        } catch (error) {
+            Swal.fire({
+                title: "Error",
+                html: "Something went wrong while creating the file:<br/>" + error,
+                icon: "error"
+            });
+        }
     }).catch(function() {
         Swal.fire({
             title: "Error",
@@ -35,47 +43,54 @@ function DownloadJSON() {
 // Create and Download voice_ban.dt
 function DownloadVoiceBan() {
     $.get(BOT_LIST_URL, function (data) {
-        if (!data) { return; }
-        let botList = FilterList(data);
-        let fileLength = 32 * botList.length + 4; // Calculate file size
+        try {
+            if (!data) { return; }
+            let botList = FilterList(data);
+            let fileLength = 32 * botList.length + 4; // Calculate file size
 
-        let vbFile = new Uint8Array(fileLength);
-        vbFile[0] = 0x01;
-        vbFile[1] = 0x00;
-        vbFile[2] = 0x00;
-        vbFile[3] = 0x00;
+            let vbFile = new Uint8Array(fileLength);
+            vbFile[0] = 0x01;
+            vbFile[1] = 0x00;
+            vbFile[2] = 0x00;
+            vbFile[3] = 0x00;
 
-        let index = 4;
-        for (let i = 0; i < botList.length; i++) {
-            let cLine = botList[i];
+            let index = 4;
+            for (let i = 0; i < botList.length; i++) {
+                let cLine = botList[i];
 
-            if (cLine.startsWith("//") || !cLine) {
-                continue;
-            }
-
-            let cSteamId3 = ID64toID3(botList[i]);
-
-            for (let j = 0; j < 32; j++) { // For each char of id (max 32)
-                if (cSteamId3.charCodeAt(j)) { // Check if char at j exist
-                    vbFile[index] = cSteamId3.charCodeAt(j); // Add char at [j] to file
-                } else {
-                    vbFile[index] = 0x00; // Add 0x00 at [j] to file
+                if (cLine.startsWith("//") || !cLine) {
+                    continue;
                 }
-                index++;
+
+                let cSteamId3 = ID64toID3(botList[i]);
+
+                for (let j = 0; j < 32; j++) { // For each char of id (max 32)
+                    if (cSteamId3.charCodeAt(j)) { // Check if char at j exist
+                        vbFile[index] = cSteamId3.charCodeAt(j); // Add char at [j] to file
+                    } else {
+                        vbFile[index] = 0x00; // Add 0x00 at [j] to file
+                    }
+                    index++;
+                }
             }
+
+            // Add 4 empty bytes at the end
+            vbFile[index] = 0x00;
+            index++;
+            vbFile[index] = 0x00;
+            index++;
+            vbFile[index] = 0x00;
+            index++;
+            vbFile[index] = 0x00;
+
+            DownloadBinFile("voice_ban.dt", vbFile);
+        } catch(error) {
+            Swal.fire({
+                title: "Error",
+                html: "Something went wrong while creating the file:<br/>" + error,
+                icon: "error"
+            });
         }
-
-        // Add 4 empty bytes at the end
-        vbFile[index] = 0x00;
-        index++;
-        vbFile[index] = 0x00;
-        index++;
-        vbFile[index] = 0x00;
-        index++;
-        vbFile[index] = 0x00;
-
-        console.log(botList);
-        DownloadBinFile("voice_ban.dt", vbFile);
     }).catch(function() {
         Swal.fire({
             title: "Error",
